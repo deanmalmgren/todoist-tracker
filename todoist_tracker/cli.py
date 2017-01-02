b5280003e42ae3ff33a352ff1bb805f3e1aaa066
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2017-01-02 14:51:54 -0600

    started to port some things from flo

--- todoist_tracker/cli.py
+++ todoist_tracker/cli.py
@@ -1,6 +1,35 @@
 import argparse
 
 
+def get_command_line_parser():
+    """Public function for creating a parser to execute all of the commands
+    in this sub-package.
+    """
+    command_line_parser = argparse.ArgumentParser(
+        description="Execute data workflows defined in flo.yaml files",
+    )
+    subcommand_creator = command_line_parser.add_subparsers(
+        title='SUBCOMMANDS',
+    )
+    for command_module in COMMAND_MODULES:
+        command = command_module.Command(subcommand_creator)
+
+        # this sets a default value for the command "option" so
+        # that, when this Command is selected by argparse from the
+        # command line, we know which common instance it
+        # corresponds with. See run_subcommand function below.
+        command.option_parser.set_defaults(command=command)
+    return command_line_parser
+
+
+def run_subcommand(args):
+    """This function runs the command that is selected by this particular
+    subcommand parser.
+    """
+    command = args.__dict__.pop("command")
+    command.execute(**args.__dict__)
+
+
 class TodoistTrackerParser(argparse.ArgumentParser):
     """This script uses the Todoist configuration in todoist.json to track
     various things in todoist and store them in a google spreadsheet.
