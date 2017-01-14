f2576a6bf5a80873bc825b3d64a1dc6aed0a145b
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2017-01-14 14:08:57 -0600

    version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(filename, 'r') as stream:
 # create the package
 setup(
     name='todoist-tracker',
-    version='0.1.2',
+    version='0.1.3',
     description="track various metrics for todoist",
     long_description=long_description,
     url=github_url,
